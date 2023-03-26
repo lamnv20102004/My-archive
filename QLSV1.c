cbe0f3808f76fffd93af6642ac64c09eec588e1e
lamnv20102004 <63687380+lamnv20102004@users.noreply.github.com>
2023-03-26 13:05:20 +0700

    Update QLSV1.c

--- QLSV1.c
+++ QLSV1.c
@@ -1,146 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-typedef struct Student {
-  int id;
-  char name[50];
-  float scores[3];
-  float total_score;
-} Student;
+typedef struct {
+    int student_code;
+    char student_name[50];
+    double subject_a;
+    double subject_b;
+    double subject_c;
+} student;
 
+student students[1000];
 int num_students = 0;
-int max_students = 100;
-Student* students;
 
-void addNewStudents() {
-  int num;
-  scanf("%d", &num);
-
-  if(num_students + num >= max_students) {
-    max_students += num;
-    students = realloc(students, max_students * sizeof(Student));
-  }
-
-  for (int i = 0; i < num; i++) {
-    scanf("%s", students[num_students].name);
-    scanf("%f", &students[num_students].scores[0]);
-    scanf("%f", &students[num_students].scores[1]);
-    scanf("%f", &students[num_students].scores[2]);
-
-    students[num_students].id = num_students + 1;
-    num_students++;
-  }
+void add_new(int num_new_students) {
+    int i;
+    for (i = 0; i < num_new_students; i++) {
+        students[num_students].student_code = num_students + 1;
+        scanf("%s %lf %lf %lf", students[num_students].student_name, &students[num_students].subject_a, &students[num_students].subject_b, &students[num_students].subject_c);
+        num_students++;
+    }
 }
 
-void updateScore() {
-  int code;
-  scanf("%d", &code);
-
-  for (int i = 0; i < num_students; i++) {
-    if (students[i].id == code) {
-      scanf("%f", &students[i].scores[0]);
-      scanf("%f", &students[i].scores[1]);
-      scanf("%f", &students[i].scores[2]);
-      return;
-    }
-  }
-}
-
-void merge(Student arr[], int left, int mid, int right) {
-    int i, j, k;
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-
-    Student L[n1], R[n2];
-    for (i = 0; i < n1; i++) {
-        L[i] = arr[left + i];
-    }
-    for (j = 0; j < n2; j++) {
-        R[j] = arr[mid + 1 + j];
-    }
-
-    i = 0;
-    j = 0;
-    k = left;
-    while (i < n1 && j < n2) {
-        if (L[i].total_score <= R[j].total_score) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
+void update_score(int student_code) {
+    int i;
+    for (i = 0; i < num_students; i++) {
+        if (students[i].student_code == student_code) {
+            scanf("%lf %lf %lf", &students[i].subject_a, &students[i].subject_b, &students[i].subject_c);
+            return;
         }
-        k++;
-    }
-
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
     }
 }
 
-void mergeSort(Student arr[], int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
-    }
+int compare_students(const void *p1, const void *p2) {
+    double score1 = (*(student*)p1).subject_a + (*(student*)p1).subject_b + (*(student*)p1).subject_c;
+    double score2 = (*(student*)p2).subject_a + (*(student*)p2).subject_b + (*(student*)p2).subject_c;
+    if (score1 > score2) return -1;
+    else if (score1 < score2) return 1;
+    else return 0;
 }
-void displayList() {
-  Student temp;
-  float total_score[num_students];
-  for (int i = 0; i < num_students; i++) {
-    total_score[i] = students[i].scores[0] + students[i].scores[1] + students[i].scores[2];
-  }
 
-  for (int i = 0; i < num_students - 1; i++) {
-    for (int j = i + 1; j < num_students; j++) {
-      if (total_score[i] > total_score[j]) {
-        temp = students[i];
-        students[i] = students[j];
-        students[j] = temp;
-        float tmp = total_score[i];
-        total_score[i] = total_score[j];
-        total_score[j] = tmp;
-      }
+void display_list() {
+    int i;
+    qsort(students, num_students, sizeof(student), compare_students);
+    for (i = 0; i < num_students; i++) {
+        double total_score = students[i].subject_a + students[i].subject_b + students[i].subject_c;
+        printf("%d %s %.2lf\n", students[i].student_code, students[i].student_name, total_score);
     }
-  }
-
-  for (int i = 0; i < num_students; i++) {
-    printf("%d %s %0.1f %0.1f %0.1f %0.1f\n", i + 1, students[i].name, students[i].scores[0], students[i].scores[1], students[i].scores[2], total_score[i]);
-  }
 }
 
 int main() {
-  students = malloc(max_students * sizeof(Student));
-
-  int choice;
-
-  do {
-    scanf("%d", &choice);
-    switch (choice) {
-      case 1:
-        addNewStudents();
-        break;
-      case 2:
-        updateScore();
-        break;
-      case 3:
-        displayList();
-        break;
-      default:
-        break;
+    int choice;
+    while (scanf("%d", &choice) != EOF) {
+        if (choice == 1) {
+            int num_new_students;
+            scanf("%d", &num_new_students);
+            add_new(num_new_students);
+        } else if (choice == 2) {
+            int student_code;
+            scanf("%d", &student_code);
+            update_score(student_code);
+        } else if (choice == 3) {
+            display_list();
+            break;
+        }
     }
-  } while (choice != 0);
-
-  return 0;
 }
